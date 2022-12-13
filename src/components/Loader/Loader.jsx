0ccf1960a23c039a93cd958b58ea047f1d462c02
Roman-Huminilovych <103421237+Roman-Huminilovych@users.noreply.github.com>
2022-12-13 15:50:52 +0200

    hw

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import s from "./Loader.module.css";
 export default function Loader() {
   return (
     <div className={s.overlay}>
-      <DotLoader size={250} color={"#461646"} className={s.loader} />
+      <DotLoader size={250} color={"#8b4513"} className={s.loader} />
     </div>
   );
 }
